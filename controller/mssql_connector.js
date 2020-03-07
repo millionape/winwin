30a6a6f217929ab91dbde9dc1a3c524e1a36f4ef
millionape <kittaporn.jp@gmail.com>
2020-03-07 22:55:16 +0700

    ijdoo

--- controller/mssql_connector.js
+++ controller/mssql_connector.js
@@ -1,70 +1,158 @@
 var sql = require('mssql');
 const randomString = require('random-base64-string')
 var sqlConfig = {
-    user: 'SA',
-    password: 'Gun11092544#',
-    server: '13.229.88.179',
-    database: 'myDatabase',
-    // options: {
-    //   encrypt: true
-    // }
-  };
+  user: 'SA',
+  password: 'Gun11092544#',
+  server: '13.229.88.179',
+  database: 'myDatabase',
+  // options: {
+  //   encrypt: true
+  // }
+};
 
-exports.getUser = function(user,pass){
-    return new Promise(async function(resolve, reject) {
-        try {
-            console.log("sql connecting......")
-            let pool = await sql.connect(sqlConfig)
-            let result = await pool.request().query('select * from users') 
-            //console.log(result.recordset)
-            for(var auth of result.recordset){
-              if(user === auth.userLogin && pass === auth.userPassword){
-                resolve(true);
-              }
-            }
-            resolve(false);
-            
-          } catch (err) {
-            console.log(err);
-            resolve(false);
-            
-          }
-    })
-}
-///////////////  GROUPS //////////////////////////////
-exports.get_all_groups = function(){
-  return new Promise(async function(resolve, reject) {
-      try {
-          console.log("sql connecting......")
-          let pool = await sql.connect(sqlConfig)
-          let result = await pool.request().query('select * from groups') 
-          //console.log(result.recordset)
-          resolve(result);
-          
-        } catch (err) {
-          console.log(err);
-          resolve(err);
-          
+exports.getUser = function (user, pass) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      console.log("sql connecting......")
+      let pool = await sql.connect(sqlConfig)
+      let result = await pool.request().query('select * from users')
+      //console.log(result.recordset)
+      for (var auth of result.recordset) {
+        if (user === auth.userLogin && pass === auth.userPassword) {
+          // res.cookie('userId', auth.userId)
+          resolve(auth.userId);
         }
+      }
+      resolve(false);
+
+    } catch (err) {
+      console.log(err);
+      resolve(false);
+
+    }
   })
 }
-exports.create_new_groups = function(groupName,inchargeName,inchargeLastName,locationText,province,amphoe,district,zipcode){
-  return new Promise(async function(resolve, reject) {
-      try {
-          console.log("sql connecting......");
-          let pool = await sql.connect(sqlConfig)
-          const generatedId = randomString(12);
-          let result = await pool.request().query(`INSERT INTO groups (groupId, groupName, inchargeName, inchargeLastName, locationInText , province , amphoe , district , zipcode) VALUES ('${generatedId}', '${groupName}', '${inchargeName}', '${inchargeLastName}','${locationText}','${province}','${amphoe}','${district}','${zipcode}')`); 
-          //console.log(result.recordset)
-          resolve(result);
-          
-        } catch (err) {
-          console.log(err);
-          resolve(err);
-          
-        }
+///////////////  GROUPS //////////////////////////////
+exports.get_all_groups = function () {
+  return new Promise(async function (resolve, reject) {
+    try {
+      console.log("sql connecting......")
+      let pool = await sql.connect(sqlConfig)
+      let result = await pool.request().query('select * from groups')
+      //console.log(result.recordset)
+      resolve(result);
+
+    } catch (err) {
+      console.log(err);
+      resolve(err);
+
+    }
   })
+}
+exports.create_new_groups = function (groupName, inchargeName, inchargeLastName, locationText, province, amphoe, district, zipcode) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      console.log("sql connecting......");
+      let pool = await sql.connect(sqlConfig)
+      const generatedId = randomString(12);
+      let result = await pool.request().query(`INSERT INTO groups (groupId, groupName, inchargeName, inchargeLastName, locationInText , province , amphoe , district , zipcode) VALUES ('${generatedId}', '${groupName}', '${inchargeName}', '${inchargeLastName}','${locationText}','${province}','${amphoe}','${district}','${zipcode}')`);
+      //console.log(result.recordset)
+      resolve(result);
+
+    } catch (err) {
+      console.log(err);
+      resolve(err);
+
+    }
+  })
+}
+exports.edit_groups = function (groupName, inchargeName, inchargeLastName, locationText, province, amphoe, district, zipcode ,groupId) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      console.log("sql connecting......");
+      let pool = await sql.connect(sqlConfig)
+      // const generatedId = randomString(12);
+      let result = await pool.request().query(`UPDATE groups SET groupName='${groupName}', inchargeName='${inchargeName}', inchargeLastName='${inchargeLastName}',locationInText='${locationText}',province='${province}',amphoe='${amphoe}',district='${district}',zipcode='${zipcode}' WHERE groupId='${groupId}'`);
+      //console.log(result.recordset)
+      resolve(result);
+
+    } catch (err) {
+      console.log(err);
+      resolve(err);
+
+    }
+  })
+}
+exports.getGroupInfo = function (groupId) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      console.log("sql connecting......");
+      let pool = await sql.connect(sqlConfig)
+      let result = await pool.request().query(`SELECT * FROM dbo.groups WHERE groupId='${groupId}'`);
+      console.log('select result :',result);
+      resolve(result);
+
+    } catch (err) {
+      console.log(err);
+      resolve(err);
+
+    }
+  });
+}
+exports.groupDelete = function (groupId) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      console.log("sql connecting......");
+      let pool = await sql.connect(sqlConfig)
+      let result = await pool.request().query(`DELETE FROM groups WHERE groupId='${groupId}'`);
+      console.log('select result :',result);
+      resolve(result);
+
+    } catch (err) {
+      console.log(err);
+      resolve(err);
+
+    }
+  });
 }
 
 
 ///////////////  GROUPS //////////////////////////////
+
+///////////////// MEMBES //////////////////////////////////
+exports.create_new_members = function (groupId, memberName, memberLastName, memberThaiId, memberMobileNumber, memberShirtNumber, motorcycleBrand, motorcycleColor,memberLicenseNumber,memberLicenseType,memberMotorcycleSerial,gender) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      console.log("sql connecting......");
+      let pool = await sql.connect(sqlConfig)
+      const generatedId = randomString(12);
+      let result = await pool.request().query(`INSERT INTO personInfo (groupId, personId, personName, personLastName, mobileNumber , shirtNumber , motorcycleBrand , motorcycleColor , licensePlate , licenseType , motorcycleSerial, gender) 
+                                              VALUES ('${groupId}', '${memberThaiId}', '${memberName}', '${memberLastName}','${memberMobileNumber}','${memberShirtNumber}','${motorcycleBrand}','${motorcycleColor}','${memberLicenseNumber}','${memberLicenseType}','${memberMotorcycleSerial}','${gender}')`);
+      //console.log(result.recordset)
+      resolve(result);
+
+    } catch (err) {
+      console.log(err);
+      resolve(err);
+
+    }
+  })
+}
+exports.list_members_in_group = function(groupId){
+  return new Promise(async function (resolve, reject) {
+    try {
+      console.log("sql connecting......");
+      let pool = await sql.connect(sqlConfig)
+      let result = await pool.request().query(`SELECT TOP 1000 * FROM dbo.personInfo WHERE groupId='${groupId}'`);
+      //console.log(result.recordset)
+      resolve(result);
+
+    } catch (err) {
+      console.log(err);
+      resolve(err);
+
+    }
+  })
+}
+
+///////////////// MEMBES //////////////////////////////////
--- routes/index.js
+++ routes/index.js
@@ -5,32 +5,82 @@ var db = require('../controller/mssql_connector')
 const fs = require('fs');
 
 var router = express.Router();
+const auth = (req, res, next) => {
+  /* ตรวจสอบว่า authorization คือ Boy หรือไม่*/
+  if (req.cookies.userId !== undefined)
+    next(); //อนุญาตให้ไปฟังก์ชันถัดไป
+  else
+    res.send("ไม่มีสิทธิในการเข้าถึง")
+};
 
-
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+  if(req.cookies.userId !== undefined){
+    res.redirect('/admin')
+  }else{
+    res.render('index');
+  }
+  
 });
-router.get('/createGroup', function(req, res, next) {
+router.get('/logout', function (req, res, next) {
+  res.clearCookie("userId");
+  res.redirect('/')
+});
+router.get('/showGroup', auth, async function (req, res, next) {
+  const groupId = req.query.id;
+  var datas = location.getLocations();
+  let members = await db.list_members_in_group(groupId);
+  console.log('list members :', members.recordset);
+  db.getGroupInfo(groupId).then(function (response) {
+    if (response) {
+      console.log('res :', response)
+      res.render('editGroup', {
+        locations: datas,
+        groupData: response.recordset[0],
+        members: members.recordset
+      });
+    }
+  })
+
+});
+router.get('/deleteGroup', auth, function (req, res, next) {
+  const groupId = req.query.groupId;
+  db.groupDelete(groupId).then(function(result){
+    if(result){
+      console.log('delete status :',result);
+      res.redirect('/admin')
+    }
+  })
+
+});
+router.get('/createGroup', auth, function (req, res, next) {
   var datas = location.getLocations();
   // const categories = [...new Set(datas.map(data => data['province']))]
   // for(var item of data){
   //   console.log(item['province']);
   // }
   // console.log(categories)
-  res.render('createGroup',{
-    locations:datas
+  res.render('createGroup', {
+    locations: datas
   });
 });
-router.get('/admin', function(req, res, next) {
-  db.get_all_groups().then(function(result){
-    console.log('groups :',result);
-    res.render('admin',{
-      groups : result.recordset
+router.get('/admin', auth, function (req, res, next) {
+  db.get_all_groups().then(function (result) {
+    console.log('groups :', result);
+    res.render('admin', {
+      groups: result.recordset
     });
-  })  
-  
+  })
 });
-router.post('/newGroupForm', function(req, res) {
+
+router.get('/addMemberToGroup', auth, function (req, res, next) {
+  const groupId = req.query.groupId;
+  const groupName = req.query.groupName;
+  res.render('createMember', {
+    groupId: groupId,
+    groupName: groupName
+  })
+});
+router.post('/newGroupForm', auth, function (req, res) {
   var groupName = req.body.groupName;
   var inchargeName = req.body.inchargeName;
   var inchargeLastName = req.body.inchargeLastName;
@@ -39,37 +89,83 @@ router.post('/newGroupForm', function(req, res) {
   var amphoe = req.body.amphoe;
   var district = req.body.district;
   var zipcode = req.body.zipcode;
-  console.log(groupName,inchargeName,
+  console.log(groupName, inchargeName,
     inchargeLastName,
     locationText,
     province,
     amphoe,
     zipcode,
     district)
-  db.create_new_groups(groupName,inchargeName,inchargeLastName,locationText,province,amphoe,district,zipcode).then(function(result){
-    console.log('insert result is',result);
+  db.create_new_groups(groupName, inchargeName, inchargeLastName, locationText, province, amphoe, district, zipcode).then(function (result) {
+    console.log('insert result is', result);
     res.redirect('/admin');
   });
 });
+router.post('/editGroupForm', auth, function (req, res) {
+  var backURL = req.header('Referer') || '/';
+  var groupName = req.body.groupName;
+  var inchargeName = req.body.inchargeName;
+  var inchargeLastName = req.body.inchargeLastName;
+  var locationText = req.body.locationText;
+  var province = req.body.province;
+  var amphoe = req.body.amphoe;
+  var district = req.body.district;
+  var zipcode = req.body.zipcode;
+  var groupId = req.body.groupId;
+  console.log(groupName, inchargeName,
+    inchargeLastName,
+    locationText,
+    province,
+    amphoe,
+    zipcode,
+    district)
+  db.edit_groups(groupName, inchargeName, inchargeLastName, locationText, province, amphoe, district, zipcode, groupId).then(function (result) {
+    console.log('edit result is', result);
+    res.redirect(backURL);
+  });
+});
 
-router.post('/login', function(req, res) {
+router.post('/login', function (req, res) {
   var user = req.body.user;
   var pass = req.body.password;
-  console.log('recv :',user,pass);
-  db.getUser(user,pass).then(function(result){
-    console.log('auth:',result);
-    if(result){
-      res.redirect('/admin')
-    }else{
-      res.render('error',{
-        title : "Auth error",
-        msg : 'username or password wrong , please try again'
-      })
-    }
-  });
+    console.log('recv :', user, pass);
+    db.getUser(user, pass).then(function (result) {
+      console.log('auth:', result);
+      if (result !== false) {
+        res.cookie('userId', result)
+        res.redirect('/admin')
+      } else {
+        res.render('error', {
+          title: "Auth error",
+          msg: 'username or password wrong , please try again'
+        })
+      }
+    });
   
+
   // ...
 });
+router.post('/newMemberForm', auth, function (req, res) {
+  var backURL = req.header('Referer') || '/';
+  var groupId = req.body.groupId;
+  var memberName = req.body.memberName;
+  var memberLastName = req.body.memberLastName;
+  var memberThaiId = req.body.memberThaiId;
+  var memberMobileNumber = req.body.memberMobileNumber;
+  var memberShirtNumber = req.body.memberShirtNumber;
+  var motorcycleBrand = req.body.motorcycleBrand;
+  var motorcycleColor = req.body.motorcycleColor;
+  var memberLicenseNumber = req.body.memberLicenseNumber;
+  var memberLicenseType = req.body.memberLicenseType;
+  var memberMotorcycleSerial = req.body.memberMotorcycleSerial;
+  var gender = req.body.gender;
+  db.create_new_members(groupId, memberName, memberLastName, memberThaiId, memberMobileNumber, memberShirtNumber, motorcycleBrand, motorcycleColor, memberLicenseNumber, memberLicenseType, memberMotorcycleSerial, gender).then(
+    function (result) {
+      console.log('new member result :', result);
+      res.redirect(`/showgroup?id=${groupId}`);
+    }
+  )
+})
 
 
 
